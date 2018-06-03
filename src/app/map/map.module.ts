69b6fac7a83e24b39f1ae217e16bb5fe2fdc56d6
Duncan McColl <duncmc831@gmail.com>
2018-06-03 09:56:54 -0700

    progress: map ui and diplay plaaceholder

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MapComponent } from './map/map.component';
+import { MapComponent } from './map.component';
 import { RouterModule } from '@angular/router';
 import { AgmCoreModule } from '@agm/core';
 import { callLifecycleHooksChildrenFirst } from '@angular/core/src/view/provider';
-import { AppRoutingModule } from './app-routing.module';
-import { GreenTableComponent } from './green-table/green-table.component';
-import { AppComponent } from './app.component'
+import { AppRoutingModule } from '../app-routing.module';
+import { GreenTableComponent } from '../green-table/green-table.component';
+
 
 @NgModule({
   imports: [
@@ -21,7 +21,7 @@ import { AppComponent } from './app.component'
     AppRoutingModule
   ],
   providers: [],
-  declarations: [ AppComponent, MapComponent, GreenTableComponent],
-  bootstrap: [ AppComponent ]
+  declarations: [ MapComponent, GreenTableComponent],
+  bootstrap: [ MapComponent ]
 })
 export class AppModule {}
